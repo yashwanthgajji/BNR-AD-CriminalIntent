209f6c8e91917fa67c9c28740c4d89033e48284b
YashwanthGajji <yashwanth.gajji333@gmail.com>
2024-04-17 16:28:33 -0400

    crime-detail-fragment

---
+++ app/src/main/java/com/yash/android/bnr/criminalintent/Crime.kt
@@ -0,0 +1,11 @@
+package com.yash.android.bnr.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime (
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
---
+++ app/src/main/java/com/yash/android/bnr/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,51 @@
+package com.yash.android.bnr.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.yash.android.bnr.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment: Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, start, before, count ->
+                crime = crime.copy(title = text.toString());
+            }
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+            crimeSolved.setOnCheckedChangeListener { buttonView, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+}
