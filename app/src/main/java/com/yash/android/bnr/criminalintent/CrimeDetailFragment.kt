687df9c9f60507dffd177da4095787b5c7895821
YashwanthGajji <yashwanth.gajji333@gmail.com>
2024-04-20 11:22:53 -0400

    binding-null-ondestroy-view-fragment

--- app/src/main/java/com/yash/android/bnr/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/yash/android/bnr/criminalintent/CrimeDetailFragment.kt
@@ -11,7 +11,11 @@ import java.util.Date
 import java.util.UUID
 
 class CrimeDetailFragment: Fragment() {
-    private lateinit var binding: FragmentCrimeDetailBinding
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
     private lateinit var crime: Crime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +32,7 @@ class CrimeDetailFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -46,5 +50,10 @@ class CrimeDetailFragment: Fragment() {
                 crime = crime.copy(isSolved = isChecked)
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
