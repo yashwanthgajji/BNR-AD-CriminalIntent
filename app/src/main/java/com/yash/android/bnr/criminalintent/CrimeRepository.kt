d3b0f94d7af405848633ad64c3cd482de973185c
YashwanthGajji <yashwanth.gajji333@gmail.com>
2024-04-28 16:54:06 -0400

    crime get functions

--- app/src/main/java/com/yash/android/bnr/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/yash/android/bnr/criminalintent/CrimeRepository.kt
@@ -1,8 +1,23 @@
 package com.yash.android.bnr.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.yash.android.bnr.criminalintent.database.CrimeDatabase
+import java.util.UUID
 
+private const val DATABASE_NAME = "crime-database"
 class CrimeRepository private constructor(context: Context){
+
+    private val crimeDatabase: CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    suspend fun getCrimes(): List<Crime> = crimeDatabase.crimeDao().getCrimes()
+
+    suspend fun getCrime(crimeId: UUID): Crime = crimeDatabase.crimeDao().getCrime(crimeId)
+
     companion object {
         private var INSTANCE: CrimeRepository ?= null
 
