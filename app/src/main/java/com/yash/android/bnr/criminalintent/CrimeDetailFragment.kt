c4b3c169ddd996fa07b5931d6a9e7d6239311e73
Yashwanth Gajji <yashwanth.gajji333@gmail.com>
2024-05-06 17:17:27 -0400

    Chapter 14 (#11)
    - Added a Date Picker dialog fragment to show crime date.
    - Passed args between detail fragment and date picker to get selected date and update it in database.

--- app/src/main/java/com/yash/android/bnr/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/yash/android/bnr/criminalintent/CrimeDetailFragment.kt
@@ -3,7 +3,6 @@ package com.yash.android.bnr.criminalintent
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +10,7 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -20,7 +20,6 @@ import androidx.navigation.fragment.navArgs
 import com.yash.android.bnr.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
 import java.util.Date
-import java.util.UUID
 
 class CrimeDetailFragment: Fragment() {
     private var _binding: FragmentCrimeDetailBinding? = null
@@ -62,9 +61,6 @@ class CrimeDetailFragment: Fragment() {
                     oldCrime.copy(title = text.toString())
                 }
             }
-            crimeDate.apply {
-                isEnabled = false
-            }
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
                 crimeDetailViewModel.updateCrime { oldCrime ->
                     oldCrime.copy(isSolved = isChecked)
@@ -78,6 +74,11 @@ class CrimeDetailFragment: Fragment() {
                 }
             }
         }
+        setFragmentResultListener(DatePickerFragment.REQUEST_KEY_DATE) { requestKey, bundle ->
+            val newDate =
+                bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
+        }
     }
 
     private fun updateUi(crime: Crime) {
@@ -86,6 +87,9 @@ class CrimeDetailFragment: Fragment() {
                 crimeTitle.setText(crime.title)
             }
             crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(CrimeDetailFragmentDirections.selectDate(crime.date))
+            }
             crimeSolved.isChecked = crime.isSolved
         }
     }
