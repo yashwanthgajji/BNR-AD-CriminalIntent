2318ccf2af44f99b88b72c0ff81111001b248cec
YashwanthGajji <yashwanth.gajji333@gmail.com>
2024-05-17 19:15:51 -0400

    migrated database version by adding suspect column

--- app/src/main/java/com/yash/android/bnr/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/yash/android/bnr/criminalintent/CrimeRepository.kt
@@ -3,6 +3,7 @@ package com.yash.android.bnr.criminalintent
 import android.content.Context
 import androidx.room.Room
 import com.yash.android.bnr.criminalintent.database.CrimeDatabase
+import com.yash.android.bnr.criminalintent.database.migration_1_2
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -19,7 +20,7 @@ class CrimeRepository private constructor(
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).build()
+    ).addMigrations(migration_1_2).build()
 
     fun getCrimes(): Flow<List<Crime>> = crimeDatabase.crimeDao().getCrimes()
 
